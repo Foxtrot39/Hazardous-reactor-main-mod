0b056e17cee697397307581f44cfdb76368de97f
Nekrodamus <paladinifranco93@gmail.com>
2023-05-18 21:01:34 -0300

    Update HazardousReactor_bothside.lua
    1. Instead of iterating over all items in Item.ItemList to modify the properties of repairables and reactors in the first hook, you can directly filter the relevant items using a filter function in combination with pairs. This will improve performance by avoiding iterating over unnecessary items.
    
    2. Applied the same filtering approach to avoid iterating over all the items in Item.ItemList and only modify the necessary reactors.
    
    3. Use the conditional assignment operator (or) to avoid repeating the verification of the existence of Level.Loaded and outpost

--- Hazardous Reactors [ Lethal radiation update  ]/Lua/Autorun/HazardousReactor_bothside.lua
+++ Hazardous Reactors [ Lethal radiation update  ]/Lua/Autorun/HazardousReactor_bothside.lua
@@ -1,35 +1,33 @@
 -- Reactor repair speed override
 Hook.Add("roundStart", "changeRepairThingyOfReactors", function()
-  for k, v in pairs(Item.ItemList) do
-     local repairable = v.GetComponentString("Repairable")
-     if repairable and v.Submarine == Submarine.MainSub and  v.GetComponentString("Reactor") then
-        repairable.RepairThreshold = 80
-        repairable.MinDeteriorationCondition = 0
-        repairable.FixDurationLowSkill = 60
-        repairable.FixDurationHighSkill = 30
-        repairable.MinDeteriorationDelay = 120
-        repairable.MaxDeteriorationDelay = 240
-     end
+  for _, v in pairs(Item.ItemList:Filter(function(item)
+    return item.Submarine == Submarine.MainSub and item.GetComponentString("Reactor")
+  end)) do
+    local repairable = v.GetComponentString("Repairable")
+    repairable.RepairThreshold = 80
+    repairable.MinDeteriorationCondition = 0
+    repairable.FixDurationLowSkill = 60
+    repairable.FixDurationHighSkill = 30
+    repairable.MinDeteriorationDelay = 120
+    repairable.MaxDeteriorationDelay = 240
   end
 end)
 
 -- Reactor fire/meltdown delay override
 Hook.Add("roundStart", "ChangeReactorMeltdownTimers", function()
-   for k, v in pairs(Item.ItemList) do
-     local reactor = v.GetComponentString("Reactor")
-     if reactor then
-        reactor.FireDelay = 15
-        reactor.MeltdownDelay= 40
-     end
+  for _, v in pairs(Item.ItemList:Filter(function(item)
+    return item.GetComponentString("Reactor")
+  end)) do
+    local reactor = v.GetComponentString("Reactor")
+    reactor.FireDelay = 15
+    reactor.MeltdownDelay = 40
   end
 end)
 
 -- Remove fuel consumption from outpost reactor
 local thoriumFuelRodPrefab = ItemPrefab.GetItemPrefab("thoriumfuelrod")
-Hook.Add("roundStart", "infinitefuel", function ()
-  if not Level.Loaded then return end
-
-  local outpost = Level.Loaded.StartOutpost
+Hook.Add("roundStart", "infinitefuel", function()
+  local outpost = Level.Loaded and Level.Loaded.StartOutpost
   if not outpost then return end
 
   for _, item in pairs(outpost.GetItems(false)) do
@@ -45,17 +43,18 @@ end)
 Hook.Patch(
   "Barotrauma.Items.Components.LightComponent",
   "UpdateBroken",
-  function (instance, ptable)
+  function(instance, ptable)
     -- Only force update if reactorfuel tag is here
     if instance.item.HasTag("reactorfuel") then
       ptable.PreventExecution = true
       instance.Update(ptable["deltaTime"], ptable["cam"])
     end
   end,
-  Hook.HookMethodType.Before)
+  Hook.HookMethodType.Before
+)
 
 -- Fuel Out for Fulgurium Fuel rod
-Hook.Add("fulguriumavailablefuel", "fulguriumfuel", function (effect, deltaTime, item, targets, worldPosition)
+Hook.Add("fulguriumavailablefuel", "fulguriumfuel", function(effect, deltaTime, item, targets, worldPosition)
   local rod = targets[1]
   if not rod then return end
 
@@ -68,7 +67,7 @@ Hook.Add("fulguriumavailablefuel", "fulguriumfuel", function (effect, deltaTime,
 end)
 
 -- Fuel Out for Fulgurium Fuel rod
-Hook.Add("incendiumavailablefuel", "incendiumfuel", function (effect, deltaTime, item, targets, worldPosition)
+Hook.Add("incendiumavailablefuel", "incendiumfuel", function(effect, deltaTime, item, targets, worldPosition)
   local rod = targets[1]
   if not rod then return end
 
